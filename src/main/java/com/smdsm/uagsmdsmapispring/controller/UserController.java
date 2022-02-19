91192da15badb3f837f550069022a78856a0412a
ivaladez-09 <ivan.valadez.0902@gmail.com>
2022-02-19 13:02:17 -0600

    Small details.

--- src/main/java/com/smdsm/uagsmdsmapispring/controller/UserController.java
+++ src/main/java/com/smdsm/uagsmdsmapispring/controller/UserController.java
@@ -2,8 +2,7 @@ package com.smdsm.uagsmdsmapispring.controller;
 
 import com.smdsm.uagsmdsmapispring.dto.UserDto;
 import com.smdsm.uagsmdsmapispring.service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +11,10 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
-
-    private static Logger log = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     UserService userService;
@@ -46,7 +44,7 @@ public class UserController {
         return new ResponseEntity<>(updatedUserDto, HttpStatus.OK);
     }
 
-    @DeleteMapping("/{userId}")
+    @DeleteMapping("/{id}")
     ResponseEntity<String> deleteUserById(@PathVariable Integer id){
         userService.deleteById(id);
         return new ResponseEntity<>("User entity deleted successfully.", HttpStatus.OK);
--- src/main/java/com/smdsm/uagsmdsmapispring/exception/GlobalExceptionHandler.java
+++ src/main/java/com/smdsm/uagsmdsmapispring/exception/GlobalExceptionHandler.java
@@ -1,8 +1,7 @@
 package com.smdsm.uagsmdsmapispring.exception;
 
 import com.smdsm.uagsmdsmapispring.dto.ErrorDetails;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -15,10 +14,9 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
-    private static Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception,
@@ -36,6 +34,7 @@ public class GlobalExceptionHandler {
                                                                WebRequest webRequest){
         ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
                 webRequest.getDescription(false));
+        log.error("Internal Server Error in " + errorDetails.getDetails() + " due to: " + errorDetails.getMessage());
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
@@ -43,11 +42,15 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception,
                                                                         WebRequest webRequest){
         Map<String, String> errors = new HashMap<>();
+        StringBuilder errorsStringBuilder = new StringBuilder();
+        errorsStringBuilder.append("\n");
         exception.getBindingResult().getAllErrors().forEach((error) ->{
             String fieldName = ((FieldError)error).getField();
             String message = error.getDefaultMessage();
+            errorsStringBuilder.append(fieldName).append(" : ").append(message).append("\n");
             errors.put(fieldName, message);
         });
+        log.error(errorsStringBuilder.toString());
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
