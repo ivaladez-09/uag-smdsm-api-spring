86c9118d37a3dab93c5d2bc3618e26daa5325a3b
ivaladez-09 <ivan.valadez.0902@gmail.com>
2022-02-27 16:11:26 -0600

    Start implementing some unit testing for Service layer and begining on Repository layer.

---
+++ src/test/java/com/smdsm/uagsmdsmapispring/persistence/repository/UserRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.smdsm.uagsmdsmapispring.persistence.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserRepositoryTest {
+
+    @Test
+    void countByGenderAndBirthdayBetween() {
+    }
+
+    @Test
+    void countByGenderAndDisease() {
+    }
+
+    @Test
+    void countByGenderAndRiskFactor() {
+    }
+}
---
+++ src/test/java/com/smdsm/uagsmdsmapispring/service/UserServiceTest.java
@@ -0,0 +1,40 @@
+package com.smdsm.uagsmdsmapispring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void countUsersByGenderAndRiskFactor() {
+    }
+
+    @Test
+    void countUsersByGenderAndDisease() {
+    }
+
+    @Test
+    void countByGenderAndBirthdayBetween() {
+    }
+}
