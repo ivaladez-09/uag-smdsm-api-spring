34d7af98b769b4da5a67fe05f8e5c234987e98df
ivaladez-09 <ivan.valadez.0902@gmail.com>
2022-03-13 13:16:44 -0600

    MOdifying SQL script to generate base data to work with in MySQL. Also, some modifications in UT. The test are not passing because of that, I will fixx them in the next commit.

--- src/test/java/com/smdsm/uagsmdsmapispring/service/UserEntityServiceTest.java
+++ src/test/java/com/smdsm/uagsmdsmapispring/service/UserEntityServiceTest.java
@@ -11,6 +11,7 @@ import static org.mockito.Mockito.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
@@ -29,10 +30,11 @@ class UserEntityServiceTest {
     private UserRepository userRepository;
     private List<UserDto> expectedAllUsersDto;
     private final Function<String, LocalDate> parser = LocalDate::parse;
-    private final ModelMapper mapper = new ModelMapper();
+    @Spy
+    private ModelMapper mapper;
 
     @InjectMocks
-    private final UserService userService = new UserServiceImpl(userRepository, mapper);
+    private UserServiceImpl userService;
 
     @BeforeEach
     public void setup(){
